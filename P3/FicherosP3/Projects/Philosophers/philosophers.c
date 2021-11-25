b600f9cc232f1ce09c123e1dcf348e61657e3227
Psikotruko <aarmor01@ucm.es>
2021-11-25 12:53:26 +0100

    Modificado el codigo de filosofos vaya puto calvo pedante de mierda

--- P3/FicherosP3/Projects/Philosophers/philosophers.c
+++ P3/FicherosP3/Projects/Philosophers/philosophers.c
@@ -3,18 +3,21 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define NR_PHILOSOPHERS 5
+#define true 1
 
-pthread_t philosophers[NR_PHILOSOPHERS];
-pthread_mutex_t forks[NR_PHILOSOPHERS];
+#define NUM_PHILOSOPHERS 5
 
+enum STATES { HUNGRY, EATING, RESTING };
 
-void init()
-{
+int philoStates[NUM_PHILOSOPHERS];
+
+pthread_t philosophers[NUM_PHILOSOPHERS];
+pthread_mutex_t forks[NUM_PHILOSOPHERS];
+
+void init() {
     int i;
-    for(i=0; i<NR_PHILOSOPHERS; i++)
-        pthread_mutex_init(&forks[i],NULL);
-    
+    for(i = 0; i < NUM_PHILOSOPHERS; i++)
+        pthread_mutex_init(&forks[i], NULL);
 }
 
 void think(int i) {
@@ -38,23 +41,33 @@ void toSleep(int i) {
     
 }
 
-void* philosopher(void* i)
-{
+void* philosopher(void* i) {
     int nPhilosopher = (int)i;
     int right = nPhilosopher;
-    int left = (nPhilosopher - 1 == -1) ? NR_PHILOSOPHERS - 1 : (nPhilosopher - 1);
-    while(1)
-    {
+    int left = (nPhilosopher - 1 == -1) ? NUM_PHILOSOPHERS - 1 : (nPhilosopher - 1);
+    while(true) {
         
         think(nPhilosopher);
         
         /// TRY TO GRAB BOTH FORKS (right and left)
+        // pthread_mutex_lock(&forks[nPhilosopher]);
+        //We verify if we're HUNGRY since later we need to put back the forks on the table
+
+        if(!pthread_mutex_trylock(&forks[right]) && !pthread_mutex_trylock(&forks[left]) ){
+            
+        }
+
+
+
 
         eat(nPhilosopher);
         
         // PUT FORKS BACK ON THE TABLE
         
         toSleep(nPhilosopher);
+        // if(philoStates[nPhilosopher] == HUNGRY && philoStates[right] != EATING && philoStates[left] != EATING){
+        //    philoStates[nPhilosopher] = EATING;
+        // }
    }
 
 }
@@ -63,10 +76,10 @@ int main()
 {
     init();
     unsigned long i;
-    for(i=0; i<NR_PHILOSOPHERS; i++)
+    for(i = 0; i < NUM_PHILOSOPHERS; i++)
         pthread_create(&philosophers[i], NULL, philosopher, (void*)i);
     
-    for(i=0; i<NR_PHILOSOPHERS; i++)
+    for(i = 0; i < NUM_PHILOSOPHERS; i++)
         pthread_join(philosophers[i],NULL);
     return 0;
 } 
